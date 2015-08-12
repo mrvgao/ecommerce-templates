3bc8cf6f2bce60a1f3fabb886424da7fb1b44b04
chenjiankum <kkltcjk@gmail.com>
2015-08-12 14:38:43 +0800

    add static & add index & add goods_detail

--- new_printer/new_printer/settings.py
+++ new_printer/new_printer/settings.py
@@ -111,3 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# static setting:important
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- new_printer/shop/views.py
+++ new_printer/shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, render
+from configuration.models import Goods
+from conf import website
 
 # Create your views here.
+
+def index(request):
+    return render(request,website.index)
+
+
+def goods_detail(request):
+    return render(request,website.goods_detail)
