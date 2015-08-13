86d00b8094e0e30a30f5ae2b128ca28c857427df
chenjiankum <kkltcjk@gmail.com>
2015-08-13 17:55:55 +0800

    remove toy_ from website

--- new_printer/configuration/website.py
+++ new_printer/configuration/website.py
@@ -24,10 +24,10 @@ if in_deploy_server:
 else:
     server = TEST_SERVER_ADDRESS
 
-toy_server_path = 'http://%(server)s:8888/static/' % {'server': server}
-toy_server_upload = 'http://%(server)s:8888/file/upload' % {'server': server}
-toy_server_download = 'http://%(server)s:8888/file/download' % {'server': server}
-toy_server_ip = 'http://%(server)s:8888/' % {'server': server}
+server_path = 'http://%(server)s:8888/static/' % {'server': server}
+server_upload = 'http://%(server)s:8888/file/upload' % {'server': server}
+server_download = 'http://%(server)s:8888/file/download' % {'server': server}
+server_ip = 'http://%(server)s:8888/' % {'server': server}
 
 stl = 'stl'
 pic = 'jpg'
