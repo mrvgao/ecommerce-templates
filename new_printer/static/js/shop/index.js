3833b9472e2e2afb5340c3b052506b7fd8ad7632
cky <576779975@qq.com>
2015-08-17 15:29:16 +0800

    none

--- new_printer/static/js/shop/index.js
+++ new_printer/static/js/shop/index.js
@@ -16,30 +16,16 @@ $(function() {//导航切换效果
 					thisParent.find('span').animate({'left':thisleft}, 400);
 					_this.addClass('mainbody-nav-current');
 					thisShowlist.empty();
-
-					$.ajax({
-						url: '/shop/filter-type',
-						type: 'GET',
-						data: { 'type': thisType,
-								'style': thisStyle
-								},
-					})
-					.done(function(res) {
+					$.post('/shop/filter-type',{ 'type': thisType,'style': thisStyle},function(data){
 						var list = JSON.parse(data).type.goods_list;
 						var listStr;
 						for(var i = 0,len=list.length;i<len;i++){
 							listStr += '<div class="list-goodsbox fl"><div class="list-picbox"><a href="'+goods[i].goods.src +'"><img src="'+goods[i].goods.preview_1 +'"alt="商品图片"/></a><div class="list-goods-name f14"><span class="fl">'+goods[i].goods.goods_name+'</span><em class="fr"></em></div></div><p class="list-goods-describe f12">'+goods[i].goods.discription +'</p><div class="list-goods-show clearfix"><img src="'+goods[i].goods.preview_1+'" alt="商品图片" class="fl"/><img src="'+goods[i].goods.preview_2+'" alt="商品图片" class="fl"/><img src="'+goods[i].goods.preview_3+' " alt="商品图片" class="fl"/></div><div class=" list-goods-info f16 clearfix"><span class="list-goods-price fl">￥'+goods[i].goods.goods_price+'</span><span class="list-goods-author fr">'+goods[i].designer+'</span></div></div>'
 						}
 						thisShowlist.append(listStr);
-					})
-					.fail(function() {
-						console.log("加载失败");
-					})
-					.always(function() {
-						console.log("请求完成");
-					});
-					
-				});				
+				});
+			});
 		});
+						
 	});
 });
