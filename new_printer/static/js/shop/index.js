110bac5959e3d3a10d25a7bae249b376de7790af
cky <576779975@qq.com>
2015-08-17 16:31:55 +0800

    none

--- new_printer/static/js/shop/index.js
+++ new_printer/static/js/shop/index.js
@@ -17,10 +17,10 @@ $(function() {//导航切换效果
 					_this.addClass('mainbody-nav-current');
 					thisShowlist.empty();
 					$.post('/shop/filter-type',{ 'type': thisType,'style': thisStyle},function(data){
-						var list = JSON.parse(data).type.goods_list;
+						var list = JSON.parse(data).goods_list;
 						var listStr;
 						for(var i = 0,len=list.length;i<len;i++){
-							listStr += '<div class="list-goodsbox fl"><div class="list-picbox"><a href="'+goods[i].goods.src +'"><img src="'+goods[i].goods.preview_1 +'"alt="商品图片"/></a><div class="list-goods-name f14"><span class="fl">'+goods[i].goods.goods_name+'</span><em class="fr"></em></div></div><p class="list-goods-describe f12">'+goods[i].goods.discription +'</p><div class="list-goods-show clearfix"><img src="'+goods[i].goods.preview_1+'" alt="商品图片" class="fl"/><img src="'+goods[i].goods.preview_2+'" alt="商品图片" class="fl"/><img src="'+goods[i].goods.preview_3+' " alt="商品图片" class="fl"/></div><div class=" list-goods-info f16 clearfix"><span class="list-goods-price fl">￥'+goods[i].goods.goods_price+'</span><span class="list-goods-author fr">'+goods[i].designer+'</span></div></div>'
+							listStr += '<div class="list-goodsbox fl"><div class="list-picbox"><a href="#"><img src="'+list[i].preview_1 +'"alt="商品图片"/></a><div class="list-goods-name f14"><span class="fl">'+list[i].name+'</span><em class="fr"></em></div></div><p class="list-goods-describe f12">'+list[i].discription +'</p><div class="list-goods-show clearfix"><img src="'+list[i].preview_1+'" alt="商品图片" class="fl"/><img src="'+list[i].preview_2+'" alt="商品图片" class="fl"/><img src="'+list[i].preview_3+' " alt="商品图片" class="fl"/></div><div class=" list-goods-info f16 clearfix"><span class="list-goods-price fl">￥'+list[i].price+'</span><span class="list-goods-author fr">'+list[i].designer_name+'</span></div></div>'
 						}
 						thisShowlist.append(listStr);
 				});
