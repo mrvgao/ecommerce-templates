201630f3c0451d8d223730a136d132a3c2cdb071
chenjiankum <kkltcjk@gmail.com>
2015-08-17 18:11:59 +0800

    none

--- new_printer/shop/utils/common_class.py
+++ new_printer/shop/utils/common_class.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 class IndexGoods(object):
     def __init__(self):
         self.goods = None
--- new_printer/utility/vender_goods_handler.py
+++ new_printer/utility/vender_goods_handler.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.core.exceptions import ObjectDoesNotExist
 from datetime import datetime,timedelta
 
