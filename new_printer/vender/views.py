2649db22face7a6f12ad62f4a2d4bf87792adaa0
chenjiankum <kkltcjk@gmail.com>
2015-08-19 14:59:27 +0800

    none

--- new_printer/vender/views.py
+++ new_printer/vender/views.py
@@ -18,17 +18,6 @@ common_handler = CommonHandler()
 
 
 def test(request):
-    vender_id = 2
-    vender = Vender_User.objects.get(id=vender_id)
-    print vender.vendername, str(vender.img)
-    bill_list = Bills.objects.filter(vender_id=vender_id)
-    for bill in bill_list:
-        bills_goods_list = Goods_Bills.objects.filter(bills_id=bill.id)
-        for bills_goods in bills_goods_list:
-            goods = Goods.objects.get(id=bills_goods.goods_id)
-            designer_name = Designer_User.objects.get(id=goods.designer_id).designername
-            vender_goods = Vender_Goods.objects.filter(goods_id=goods.id).get(vender_id=vender_id)
-            print goods.goods_name, designer_name, bill.id, bill.bill, vender_goods.download_time
     return render(request, website.test)
 
 
@@ -37,6 +26,7 @@ def vender_center(request):
     class VenderBills(object):
 
         def __init__(self):
+            self.goods_id = None
             self.goods_name = None
             self.goods_price = None
             self.goods_img = None
@@ -46,23 +36,28 @@ def vender_center(request):
             self.download_time = None
 
         def set_vender_bills(self, vender_bill):
-            self.goods_name = vender_bill[0]
-            self.goods_price = vender_bill[1]
-            self.goods_img = vender_bill[2]
-            self.description = vender_bill[3]
-            self.designer_name = vender_bill[4]
-            self.bill_id = vender_bill[5]
-            self.download_time = vender_bill[6]
+            self.goods_id = vender_bill[0]
+            self.goods_name = vender_bill[1]
+            self.goods_price = vender_bill[2]
+            self.goods_img = vender_bill[3]
+            self.description = vender_bill[4]
+            self.designer_name = vender_bill[5]
+            self.bill_id = vender_bill[6]
+            self.download_time = vender_bill[7]
 
     def get_bills_goods_information(bills_goods_list, vender_id, bill):
         vender_bills_list = []
         for bills_goods in bills_goods_list:
             goods = Goods.objects.get(id=bills_goods.goods_id)
-            designer_name = Designer_User.objects.get(id=goods.designer_id).designername
+
+            designer = Designer_User.objects.get(id=goods.designer_id)
+            designer_name = designer.designername
             vender_goods = Vender_Goods.objects.filter(goods_id=goods.id).get(vender_id=vender_id)
-            print goods.goods_name, goods.goods_price, common_handler.get_file_path(goods.preview_1), goods.description,  designer_name, bill.id, bill.bill, vender_goods.download_time
             vender_bills = VenderBills()
-            vender_bills_param = (goods.goods_name, goods.goods_price, common_handler.get_file_path(goods.preview_1), goods.description, designer_name, bill.bill, vender_goods.download_time)
+            vender_bills_param = (goods.id, goods.goods_name, goods.goods_price,
+                                  common_handler.get_file_path(goods.preview_1),
+                                  goods.description, designer_name, bill.bill,
+                                  vender_goods.download_time)
             vender_bills.set_vender_bills(vender_bills_param)
             vender_bills_list.append(vender_bills)
         return vender_bills_list
@@ -76,16 +71,16 @@ def vender_center(request):
     vender_id = 2
 
     vender = Vender_User.objects.get(id=vender_id)
-    print vender.vendername, str(vender.img)
 
     bill_list = Bills.objects.filter(vender_id=vender_id)
 
     vender_bills_list = get_bills_information(bill_list, vender_id)
 
     context = {
-        'vender_name': vender.vendername, 'vender_img': str(vender.img),
+        'vender_name': vender.vendername, 'vender_img': common_handler.get_file_path(str(vender.img)),
         'vender_bills_list': vender_bills_list,
     }
+
     return render(request, website.vender_center, context)
 
 
