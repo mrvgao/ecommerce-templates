1f6d1a380f81ccf0157a876c84d87040b57db80e
Wellaiyo <liwanglin_1994@163.com>
2015-08-21 14:02:05 +0800

    clean up the list.html

--- new_printer/static/js/shop/list.js
+++ new_printer/static/js/shop/list.js
@@ -3,6 +3,8 @@ $(function (){
 	var filter_btn = $('.filter-btn'),
 		classify_btn = $('.classify-btn'),
 		list_wrap = $('.list-wrap'),
+		guide_btn = $('.classify-guide'),
+		classify_nav_box = $('.classify-nav-box'),
 		_kind = list_wrap.attr('data-kind'),
 		_node = "<div class='list-box pr'>" + $('.list-box').html() + "</div>";
 	
@@ -50,7 +52,20 @@ $(function (){
 			filter_type: _filter,
 			classify_type: _classify
 		},function (e){
-			console.log(e);
+			list_wrap.html('');
+			for(var i=0;i<e.length;i++){
+				list_wrap.append(_node);
+
+				// 添加数据
+				$('.list-box').eq(i).find('.goods_img').attr('src',e[i].goods_img);
+				$('.list-box').eq(i).find('.goods_downloadNum').text(e[i].goods_downloadNum);
+				$('.list-box').eq(i).find('.goods_markNum').text(e[i].goods_markNum);
+				$('.list-box').eq(i).find('.goods_name').text(e[i].goods_name);
+				$('.list-box').eq(i).find('.goods_price').text(e[i].goods_price);
+				if(e[i].goods_mark){
+					$('.list-box').eq(i).find('.list-mark-btn').addClass('active');
+				}	
+			}
 		});
 		
 	});
@@ -61,5 +76,21 @@ $(function (){
 		obj.addClass('active');
 	}
 
+	guide_btn.on('click',function (){
+		var _this = $(this);
+		if(classify_nav_box.css('display') == 'none'){
+			classify_nav_box.show();
+			classify_nav_box.animate({'margin-right':0},function (){
+				_this.addClass('active').text('收起');
+				$('.guide-ico').addClass('active');
+			});
+		}else {
+			classify_nav_box.animate({'margin-right':'-343px'},function (){
+				_this.removeClass('active').text('风格筛选');
+				$('.guide-ico').removeClass('active');
+				classify_nav_box.hide();
+			});
+		}
+	});
 
 })
