51be2eb7992ac3760f3a9ddcd5433481ccd27cfe
cky <576779975@qq.com>
2015-08-21 11:18:28 +0800

    none

--- new_printer/static/js/designer/designer.js
+++ new_printer/static/js/designer/designer.js
@@ -7,7 +7,19 @@ $(document).ready(function() {
 
 	works_wait_btn.on('click',function(){//未审核页面
 		designer_works_lists.empty();
-		var waitStr = '';
+		var waitStr = '<table class="designer-works-wait" cellpadding="0" cellspacing="0"><tr><th><span>作品名称</span></th><th><span>文件类型｜文件大小</span></th><th><span>上传时间</span></th><th colspan="2">操作</th></tr>';
+		$.post('designer/photo_not_review', {"page":1}, function(e) {
+			if(e){
+				var waitList = JSON.parse(e).list;
+				for(var i=0,len=waitList.length;i<len;i++){
+					waitStr+='<tr><td><span>'+waitList[i].name+'</span></td><td><span>'+waitList[i].type+'｜'+waitList[i].size+'</span></td><td><span>'+waitList[i].date+'</span></td><td><span><button class="go-setprice">去定价</button></span></td><td></span>删除<input type="checkbox" class="works-wait-delete-check"></span></td></tr>';
+				}
+				waitStr +='<div class="designer-works-deleteAll"><button class="works-deleteAll-btn">批量删除</button><label for="checkall">全选</label><input type="checkbox" class="works-delete-allcheck" id="checkall"/></div></table>';
+			}else{
+				waitStr ='数据加载失败...';
+			}
+			designer_works_lists.append(waitStr);
+		});
 	});
 
 	works_on_btn.on('click',function(){//审核中页面
