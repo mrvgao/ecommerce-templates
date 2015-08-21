0e519d2a915d3274ae4eada63e8bd385a65c3761
cky <576779975@qq.com>
2015-08-21 21:34:45 +0800

    none

--- new_printer/static/js/designer/designer-works.js
+++ new_printer/static/js/designer/designer-works.js
@@ -9,34 +9,8 @@ $(document).ready(function(){
 	workd_unexecute(1);
 	
 
-	$('#checkall').on('click',function(){
+	$('#checkall').on('click',function(){//全选按钮点击事件
 		isCheckAll();
-	});
-
-	$('.designer-works-page li').each(function(index, el) {
-		var _this = $(this),
-			_total = _this.parent('ul').attr('data-total');
-		_this.on('click',function(){
-			var toPage = _this.text(),
-				thisType = $('.works-current').text();
-			if(toPage=="首页"){
-				toPage=1;
-			}else if(toPage=="末页"){
-				toPage=_total;
-			}else if(toPage=="..."){
-				return false;
-			}
-			
-			if(thisType=="未审核"){
-				workd_unexecute(toPage);
-			}else if(thisType=="审核中"){
-				auditing(toPage);
-			}else if(thisType=="未通过"){
-				published(toPage);
-			}else if(thisType=="已发布"){
-				not_passed(toPage);
-			}
-		});
 	});
 
 	works_wait_btn.on('click',function(){//未审核按钮点击
@@ -61,7 +35,58 @@ $(document).ready(function(){
 
 });
 
+function creatPages(){//生成页码
+	$('.designer-works-page li').on('click',function(){
+		var _this = $(this);
+		var toPage = _this.text(),
+			thisType = $('.works-current').text().substr(0,3),
+			curPage = $('.page-current').text();
+			totalPage = $('.designer-works-page').attr('data-total');
 
+		toPage = judgePage(toPage,curPage,totalPage);
+		if(toPage){
+			if(thisType=="未审核"){
+				workd_unexecute(toPage);
+			}else if(thisType=="审核中"){
+				auditing(toPage);
+			}else if(thisType=="未通过"){
+				published(toPage);
+			}else if(thisType=="已发布"){
+				not_passed(toPage);
+			}
+		}
+	});
+}
+
+function judgePage(toPage,curPage,totalPage){//判断点击的页码
+	var curPage = parseInt(curPage);
+	if(isNaN(parseInt(toPage))){
+		switch(toPage){
+			case "首页": return 1;
+			case "末页": return totalPage;
+			case "...": return false;
+		}
+		if(toPage=="下一页"){
+			if(curPage<totalPage){
+				console.log(curPage+1);
+				return (curPage+1);
+			}else{
+				alert("没有下一页啦!");
+				return false;
+			}
+		}else if(toPage=="上一页"){
+			if(curPage!=1){
+				return curPage-1;
+			}else{
+				alert("没有上一页啦!");
+				return false;
+			}
+		}
+	}else{
+		return toPage;
+	}
+
+}
 function addWorkBtnCurrent(_this){
 	var designer_works_btn = $('.designer-works-btn').find('button');
 	if(_this.hasClass('works-current')){
@@ -74,38 +99,44 @@ function addWorkBtnCurrent(_this){
 
 function workd_unexecute(page){//加载未审核的数据
 	var designer_works_lists = $('.designer-works-lists');
+	var designer_works_page =$('.designer-works-page');
+	designer_works_page.remove();
 	designer_works_lists.empty();
 	var waitStr = '<table class="designer-works-wait" cellpadding="0" cellspacing="0"><tr><th><span>作品名称</span></th><th><span>文件类型｜文件大小</span></th><th><span>上传时间</span></th><th colspan="2">操作</th></tr>';
-	$.post('/designer/workd_unexecute',{"page":0}, function(e) {
+	$.post('/designer/workd_unexecute',{"page":page}, function(e) {
 		if(e){
 			var waitList = JSON.parse(e).all_list;
 			var totalPage = JSON.parse(e).total_pages;
 			for(var i=0,len=waitList.length;i<len;i++){
-				waitStr+='<tr data-id="'+waitList[i].id+'"><td><span>'+waitList[i].name+'</span></td><td><span>'+waitList[i].type+'文件 ｜'+waitList[i].file_size+'M </span></td><td><span>'+waitList[i].upload_time+'</span></td><td><span><button class="go-setprice">去定价</button></span></td><td></span>删除<input type="checkbox" class="works-wait-delete-check"></span></td></tr>';
+				waitStr+='<tr data-id="'+waitList[i].id+'"><td><span>'+waitList[i].name+'</span></td><td><span>'+waitList[i].type+'文件 ｜'+waitList[i].file_size+'M </span></td><td><span>'+waitList[i].upload_time+'</span></td><td><span><button class="go-setprice">去定价</button></span></td><td></span><a href="javascript:void(0)" class="wait-delete-single">删除</a><input type="checkbox" class="works-wait-delete-check"></span></td></tr>';
 			}
 			waitStr +='<div class="designer-works-deleteAll"><button class="works-deleteAll-btn" onclick="deleteAll()">批量删除</button><label for="checkall">全选</label><input type="checkbox" class="works-delete-allcheck" id="checkall" onclick="isCheckAll(this)"/></div></table>';
-		designer_works_container.append(getPage(totalPage,page));
+			getPage(totalPage,page);
 		}else{
 			waitStr ='数据加载失败...';
 		}
 
 		designer_works_lists.append(waitStr);
+		deleteSigle();
 	});
 }
 
 function auditing(page){//加载审核中的数据
+	var designer_works_page =$('.designer-works-page');
+	designer_works_page.remove();
 	designer_works_lists.empty();
 	var onStr='';
 	$.post('/designer/auditing',{"page":page}, function(e) {
 		if(e){
 			var onList = JSON.parse(e).all_list;
+			var totalPage = JSON.parse(e).total_pages;
 			for(var i=0,len=onList.length;i<len;i++){
 				onStr+='<div class="designer-works-list-box clearfix" data-id="'+onList[i].id+'"><div class="designer-works-list-bigpic fl"><img src="'+onList[i].pic[0]+'"/></div><div class="designer-works-list-detail fl"><p class="designer-works-list-title">"'+onList[i].name+'"</p><p class="designer-works-list-describe">'+onList[i].describe+'</p><div class="designer-works-list-pics clearfix">';
 				for(var j=0,jlen=onList[i].pic.length;j<jlen;j++){
 					waitStr +='<img src="'+onList[i].pic[j]+'"/>';
 				}
 				onStr +='</div></div><div class="designer-works-list-status fl"><strong>审核中···</strong><p>您的作品预计在'+onList[i].restdate+'天内被审核完毕并发布。</p></div></div>';
-
+			getPage(totalPage,page);
 			}
 		}else{
 			onStr='信息加载失败..';
@@ -115,11 +146,14 @@ function auditing(page){//加载审核中的数据
 }
 
 function published(page){//获取已发布数据
+	var designer_works_page =$('.designer-works-page');
+	designer_works_page.remove();
 	designer_works_lists.empty();
 	var sucStr ='';
 	$.post('/designer/has_published',{"page":page}, function(e) {
 		if(e){
 			var sucList = JSON.parse(e).all_list;
+			var totalPage = JSON.parse(e).total_pages;
 			for(var i=0,len=sucList.length;i<len;i++){
 				sucList+='<div class="designer-works-list-box clearfix" data-id="'+sucList[i].id+'"><div class="designer-works-list-bigpic fl"><img src="'+sucList[i].pic[0]+'"/></div><div class="designer-works-list-detail fl"><p class="designer-works-list-title">"'+sucList[i].name+'"</p><p class="designer-works-list-describe">'+sucList[i].describe+'</p><div class="designer-works-list-pics clearfix">';
 				for(var j=0,jlen=sucList[i].pic.length;j<jlen;j++){
@@ -128,6 +162,7 @@ function published(page){//获取已发布数据
 				sucStr +='</div></div><div class="designer-works-list-data fl"><div class="list-data-container clearfix"><div class="list-data-box fl"><span class="list-data-num list-download-num">'+sucList[i].downloadNum+'</span>次下载</div><div class="list-data-box fl"><span class="list-data-num list-collection-num">'+sucList[i].collectionNum+'</span>次收藏</div></div><div class="list-data-price">售价：<span class="list-data-price-num">'+sucList[i].price+'</span>RMB</div><div class="list-data-update">发布时间：<span class="list-data-update-num">'+sucList[i].update+'</span></div></div><div class="designer-works-modify fl"><button class="works-modify-btn ">编辑</button><button class="works-cancel-btn">取消发布</button><input type="checkbox" class="works-cancel-check"/></div></div>'
 			}
 			sucStr+='<div class="designer-works-canelAll"><button class="works-canelAll-btn">批量取消发布</button><label for="checkall">全选</label><input type="checkbox" class="works-cancel-allcheck" id="checkall"/></div>';
+			getPage(totalPage,page);
 		}else{
 			sucStr='信息加载失败..';
 		}
@@ -136,26 +171,32 @@ function published(page){//获取已发布数据
 }
 
 function not_passed(page){//获取未通过数据
+	var designer_works_page =$('.designer-works-page');
+	designer_works_page.remove();
 	designer_works_lists.empty();
 	var notStr ='';
 	$.post('/designer/not_passed', {"page":page}, function(e){
 		if(e){
+			var data = JSON.parse(e);
 			var notList = JSON.parse(e).all_list;
-			console.log(notList.length);
+			var totalPage = JSON.parse(e).total_pages;
 			for(var i=0,len=notList.length;i<len;i++){
-			notStr+='</div><div class="designer-works-list-box clearfix" data-id="'+notList[i].id+'"><div class="designer-works-list-bigpic fl"><img src="'+notList[i].pic[0]+'" /></div><div class="designer-works-list-smdetail fl"><p class="designer-works-list-title">'+notList[i].name+'</p><p class="designer-works-list-describe">'+notList[i].description+'</p><div class="designer-works-list-pics clearfix">';
+			notStr+='</div><div class="designer-works-list-box clearfix" data-id="'+notList[i].id+'" data-type="'+notList[i].type+'" data-size="'+notList[i].file_size+'" data-price="'+notList[i].price+'" data-uptime="'+notList[i].upload_time+'"><div class="designer-works-list-bigpic fl"><img src="'+notList[i].pic[0]+'" class="works-list-bigpic" /></div><div class="designer-works-list-smdetail fl"><p class="designer-works-list-title">'+notList[i].name+'</p><p class="designer-works-list-describe">'+notList[i].description+'</p><div class="designer-works-list-pics clearfix">';
 				var picList=notList[i].pic;
 				console.log(picList.length)
 				for(var j=0,jlen=picList.length;j<jlen;j++){
-					notStr +='<img src="'+picList[j]+'"/>';
+					notStr +='<img src="'+picList[j]+'" class="designer-works-list-img" data-pid="'+j+'"/>';
 				}
 
 				notStr+='</div></div><div class="designer-works-list-data fl"><div class="works-not-container clearfix"><p class="works-not-explain"><span>未通过说明:</span>'+notList[i].not_passed+'</p><p class="works-not-time fr">'+notList[i].modify_time+'</p></div></div><div class="designer-works-modify fl"><button class="works-modify-btn ">编辑</button><button class="works-cancel-btn">取消发布</button><input type="checkbox" class="works-cancel-check"/></div></div>';
 			}
+			notStr +='<div class="designer-works-cancelAll"><button class="works-cancelAll-btn" onclick="cancelAll()">批量取消发布</button><label for="checkall">全选</label><input type="checkbox" class="works-cancel-allcheck" id="checkall" onclick="isCheckAll(this)"/></div></table>';
+			getPage(totalPage,page);
 		}else{
 			notStr='信息加载失败';
 		}
 		designer_works_lists.append(notStr);
+		edit();
 	});
 }
 
@@ -194,11 +235,58 @@ function deleteAll(){//批量删除函数
 		worksContainer.append(str);
 	}
 }
+function deleteSigle(){//单个删除
+	$('.wait-delete-single').on('click',function(){
+		var _this =$(this),
+			deleteObj = _this.parents('tr');
+			_id = deleteObj.attr('data-id');
+		$.post('/designer/unexecute_delete', {"id":_id}, function(e){
+			if(e){
+				alert(e);
+				deleteObj.remove();
+			}
+		});
+	});
+}
+function cancelAll(){//批量取消发布
+	var cancelTag = $('.works-cancel-check:checked'),
+		worksList = $('.designer-works-list-box'),
+		worksContainer = designer_works_lists;
+	var str ='没有数据啦⊙.⊙',
+		rest = worksList.length - cancelTag.length;
+	cancelTag.each(function(index, el) {
+		var _this = $(this),
+			_id = _this.parents('.designer-works-list-box').attr('data-id');
+		$.post('/designer/unexecute_delete', {"id":_id}, function(e){
+
+			if(e){
+			}
+		});
+		_this.parents('.designer-works-list-box').remove();
+	});
+	$('#checkall').attr('checked',false);
+	if(rest==0){
+		worksContainer.append(str);
+	}
+}
+
+function cancelSigle(){//单个取消发布
+	$('.works-cancel-btn').on('click',function(){
+		var _this =$(this),
+			deleteObj = _this.parents('.designer-works-list-box');
+			_id = deleteObj.attr('data-id');
+		$.post('/designer/unexecute_delete', {"id":_id}, function(e){
+			if(e){
+				alert(e);
+				deleteObj.remove();
+			}
+		});
+	});
+}
 
 function getPage(total,cur){//生成页码
 	var pageStr ='<ul class="designer-works-page" data-total="'+ total+'"><li class="designer-works-page-first">首页</li><li class="designer-works-page-prev">上一页</li>';
-	console.log(total);
-	if(total<=6){//当总页码小于6
+	if(total<=6&&total!=1){//当总页码小于6
 		for(var p=0;p<total;p++){
 			if((p+1) == cur){
 				pageStr +='<li class="page-current">'+cur+'</li>';
@@ -229,8 +317,65 @@ function getPage(total,cur){//生成页码
 		if((total-cur)>2){//当前页在倒数第二页之前，最后出现小点点
 			pageStr +='<li class="designer-works-page-dots">...</li>';
 		}
+	}else{
+		return false;
 	}
 	pageStr +='<li class="designer-works-page-next">下一页</li><li class="designer-works-page-last">末页</li></ul>';
 
-	return pageStr;
+	designer_works_container.append(pageStr);
+	creatPages();
+}
+
+function edit(data){//编辑弹窗函数
+	$('.works-modify-btn').on('click',function(){
+		var _this =$(this);
+			_parent = _this.parents('.designer-works-list-box'),
+			id = _parent.attr('data-id');
+			pic = _parent.find('.works-list-bigpic').attr('src'),
+			type = _parent.attr('data-type'),
+			size = _parent.attr('data-size'),
+			price = _parent.attr('data-price'),
+			name = _parent.find('.designer-works-list-title').text(),
+			describe = _parent.find('.designer-works-list-describe').text(),
+			imgs = _parent.find('.designer-works-list-img'),
+			modify_imgs_container = $('.modify-imgs-container'),
+			imgStr = '',
+			up_time = _parent.attr('data-uptime');
+			modify_imgs_container.empty();
+		$('.designer-zoom').css('display','block');
+		$('.modify-content').css('display','block');
+		$('.modify-stl-preview').attr('src',pic );
+		$('.modify-type').text(type);
+		$('.modify-size').text(size);
+		$('.modify-update').text(up_time);
+		$('.modify-price').val("￥"+price);
+		$('.modify-name').val(name);
+		$('.modify-describe').text(describe);
+		for(var i=0;i<imgs.length;i++){
+			var imgsrc = imgs.eq(i).attr('src');
+			console.log(imgsrc);
+			imgStr += '<div class="modify-imgs-box fl" id="imageDiv'+i+'"><img src="'+imgsrc+'" class="modify-imgs"/><div class="modify-imgs-modify"><div class="modify-imgs-modify-hidden"><input type="file" /></div><a href="javascript:void(0)" class="modify-imgs-modify-btn">修改</a><a href="javascript:void(0)" class="modify-imgs-delete-btn">删除</a></div></div>';
+		}
+		$('.modify-imgs-container').append(imgStr);
+		$('.modify-imgs-delete-btn').on('click',function(){//删除图片
+			var imgBox = $('.modify-imgs-box');
+			if(imgBox.length==1){
+				alert("至少要有一张预览图");
+			}else{
+				var _this = $(this),
+					deleteObj = _this.parents('.modify-imgs-box'),
+					picId = deleteObj.find('img').attr('data-pid');//pic分别是0，1，2
+				$.post('designer/deletePic', {"picId":picId,"id":id});
+				deleteObj.remove();
+			}
+		});
+		$('.modify-imgs-modify-btn').on('click',function(){
+
+		});
+	});
+	
+	$('.modify-container-close').on('click',function(){
+		$('.designer-zoom').css('display','none');
+		$('.modify-content').css('display','none');
+	});
 }
