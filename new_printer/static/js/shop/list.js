4a8ba3c9690aeb5babe690ba559845a3bc6c4828
Wellaiyo <liwanglin_1994@163.com>
2015-08-21 13:28:15 +0800

    finished list.html , change the footer.html and paging.html by well

--- new_printer/static/js/shop/list.js
+++ new_printer/static/js/shop/list.js
@@ -1,32 +1,65 @@
-$(function() {//导航切换效果
-	var navlist = $('.mainbody-nav-ul');
+$(function (){
 
-		navlist.each(function(index, el) {
-			var navlist = $(this).find('li');
-			navlist.each(function(index, el) {
-				$(this).on('click',function(e){
-					var _this = $(this),
-						thisParent = _this.parent('ul'),
-						thisShowlist = _this.parent().parent().siblings('.mainbody-list'),
-						thisType = thisParent.siblings('h2').text(),
-						thisStyle = _this.text();
-						console.log(thisType+'  '+thisStyle);
-					thisParent.children('li').removeClass('mainbody-nav-current');
-					var thisleft = 105*(index%5);
-					thisParent.find('span').animate({'left':thisleft}, 400);
-					_this.addClass('mainbody-nav-current');
-					thisShowlist.empty();
-					$.post('/shop/filter-type',{ 'type': thisType,'style': thisStyle},function(data){
-						var list = JSON.parse(data).goods_list;
-						var listStr='';
-						for(var i = 0,len=list.length;i<len;i++){
+	var filter_btn = $('.filter-btn'),
+		classify_btn = $('.classify-btn'),
+		list_wrap = $('.list-wrap'),
+		_kind = list_wrap.attr('data-kind'),
+		_node = "<div class='list-box pr'>" + $('.list-box').html() + "</div>";
+	
+	// 过滤筛选
+	filter_btn.on('click',function (){
+		var _this = $(this),
+			_filter = _this.attr('data-tag'),
+			_classify = classify_btn.filter('.active').attr('data-tag');
 
-							listStr += '<div class="list-goodsbox fl"><div class="list-picbox"><a href="#"><img src="'+list[i].preview_1 +'"alt="商品图片"/></a><div class="list-goods-name f14"><span class="fl">'+list[i].name+'</span><em class="fr"></em></div></div><p class="list-goods-describe f12">'+list[i].discription +'</p><div class="list-goods-show clearfix"><img src="'+list[i].preview_1+'" alt="商品图片" class="fl"/><img src="'+list[i].preview_2+'" alt="商品图片" class="fl"/><img src="'+list[i].preview_3+' " alt="商品图片" class="fl"/></div><div class=" list-goods-info f16 clearfix"><span class="list-goods-price fl">￥'+list[i].price+'</span><span class="list-goods-author fr">'+list[i].designer_name+'</span></div></div>'
-						}
-						thisShowlist.append(listStr);
-				});
-			});
+		clickFocus(_this);
+
+		$.post('getGoods/filter',{
+			list_type: _kind,
+			filter_type: _filter,
+			classify_type: _classify
+		},function (e){
+			list_wrap.html('');
+			for(var i=0;i<e.length;i++){
+				list_wrap.append(_node);
+
+				// 添加数据
+				$('.list-box').eq(i).find('.goods_img').attr('src',e[i].goods_img);
+				$('.list-box').eq(i).find('.goods_downloadNum').text(e[i].goods_downloadNum);
+				$('.list-box').eq(i).find('.goods_markNum').text(e[i].goods_markNum);
+				$('.list-box').eq(i).find('.goods_name').text(e[i].goods_name);
+				$('.list-box').eq(i).find('.goods_price').text(e[i].goods_price);
+				if(e[i].goods_mark){
+					$('.list-box').eq(i).find('.list-mark-btn').addClass('active');
+				}	
+			}
 		});
-						
+
 	});
-});
+
+	// 风格筛选
+	classify_btn.on('click',function (){
+		var _this = $(this),
+			_classify = _this.attr('data-tag'),
+			_filter = filter_btn.filter('.active').attr('data-tag');
+
+		clickFocus(_this);
+
+		$.post('getGoods/classify',{
+			list_type: _kind,
+			filter_type: _filter,
+			classify_type: _classify
+		},function (e){
+			console.log(e);
+		});
+		
+	});
+
+	// 点击切换效果
+	function clickFocus(obj){
+		obj.siblings().removeClass('active');
+		obj.addClass('active');
+	}
+
+
+})
