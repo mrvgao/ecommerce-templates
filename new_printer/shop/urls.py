723f759ed507829708a6792824d9d28f72d02ea2
Wellaiyo <liwanglin_1994@163.com>
2015-08-21 10:02:19 +0800

    change the urls

--- new_printer/shop/urls.py
+++ new_printer/shop/urls.py
@@ -6,6 +6,24 @@ urlpatterns = patterns('',
         # 首页
         url('^$', views.list, name="index"),
 
+        # 商品列表(戒指)
+        url('^list/ring$', views.list, name="list"),
+        
+        # 商品列表(吊坠)
+        url('^list/pendant$', views.list, name="list"),
+        
+        # 商品列表(耳坠)
+        url('^list/earbob$', views.list, name="list"),
+        
+        # 商品列表(手链)
+        url('^list/bracelet$', views.list, name="list"),
+
+        # 商品列表(项链)
+        url('^list/torque$', views.list, name="list"),
+
+        # 商品列表(胸针)
+        url('^list/brooch$', views.list, name="list"),
+
         # 商品详情页
         url('goods_detail', views.goods_detail, name="goods_detail"),
 
