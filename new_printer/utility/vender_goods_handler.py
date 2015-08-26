bed44f184043e4875bfe70bb1123d39db2990a97
chenjiankum <kkltcjk@gmail.com>
2015-08-26 10:40:28 +0800

    modify social_handler & vender_goods_handler

--- new_printer/utility/vender_goods_handler.py
+++ new_printer/utility/vender_goods_handler.py
@@ -17,17 +17,44 @@ class VenderGoodsHandler(object):
         return element
 
 
-    def set_is_collected(self,vender_goods,is_collected):
-        vender_goods.is_collected = is_collected
-        vender_goods.save()
+    def set_element(self, vender_id, goods_id):
+        vender_goods = Vender_Goods.objects.create(vender_id=vender_id, goods_id=goods_id)
+        return vender_goods
 
+
+    def set_is_collected(self, vender_id, goods_id, is_collected):
+        vender_goods = self.get_element(vender_id, goods_id)
+        if vender_goods:
+            vender_goods.is_collected = is_collected
+            vender.goods.save()
+        else:
+            vender_goods = Vender_Goods.objects.create(vender_id=vender_id, goods_id=goods_id,is_collected=is_collected)
 
     def get_is_collected(self, goods_id, vender_id):
         vender_goods = self.get_element(vender_id, goods_id)
         if vender_goods:
             return vender_goods.is_collected
         else:
+            vender_goods = Vender_Goods.objects.create(vender_id=vender_id, goods_id=goods_id,is_collected=False)
             return False
+
+
+    def get_collected_time(self, vender_id, goods_id):
+        vender_goods = self.get_element(vender_id, goods_id)
+        if vender_goods:
+            return vender_goods.collected_time
+        else:
+            vender_goods = Vender_Goods.objects.create(vender_id=vender_id,goods_id=goods_id)
+            return None
+
+
+    def set_collected_time(self,vender_id, goods_id):
+        vender_goods = self.get_element(vender_id, goods_id)
+        if vender_goods:
+            vender_goods.collected_time = datetime.now()
+            vender_goods.save()
+        else:
+            vender_goods = Vender_Goods.objects.create(vender_id=vender_id, goods_id=goods_id, collected_time=datetime.now())
 
 
     def get_is_buy(self, goods_id, vender_id):
@@ -35,9 +62,5 @@ class VenderGoodsHandler(object):
         if vender_goods:
             return vender_goods.is_buy
         else:
+            vender_goods = Vender_Goods.objects.create(vender_id=vender_id, goods_id=goods_id, is_buy=False)
             return False
-
-
-    def set_collected_time(self,vender_goods):
-        vender_goods.collected_time = datetime.now()
-        vender_goods.save()
