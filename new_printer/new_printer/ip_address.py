645528351cb84b08d5fb720a2d22fbaa518e3bd2
Wellaiyo <liwanglin_1994@163.com>
2015-08-26 09:35:52 +0800

    change ip address test

---
+++ new_printer/new_printer/ip_address.py
@@ -0,0 +1,30 @@
+import socket
+
+def in_test_server():
+    '''
+    tests whether this project is running on the test server. 
+    if run in the test server, its ip address will be '127***' or '192'
+
+    Args:
+      None
+    Return:
+      result: Boolean, test if in the test server or deploy server. 
+              if in test server, return *True*, if in deploy server, 
+              return *False*
+    '''
+    ip_address = socket.gethostbyname(socket.gethostname())
+    print(ip_address)
+    
+    key_words = ['127', '192']
+
+    result = False
+    for word in key_words:
+      if ip_address.startswith(word):
+        result = True
+        break
+
+    return result
+
+
+if __name__ == '__main__':
+  print(in_test_server())
