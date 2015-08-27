ecb4410603521e678b46bc4bb2738586bee5be95
chenjiankum <kkltcjk@gmail.com>
2015-08-27 10:14:08 +0800

    remove print from ip_address

--- new_printer/new_printer/ip_address.py
+++ new_printer/new_printer/ip_address.py
@@ -2,19 +2,18 @@ import socket
 
 def in_test_server():
     '''
-    tests whether this project is running on the test server. 
+    tests whether this project is running on the test server.
     if run in the test server, its ip address will be '127***' or '192'
 
     Args:
       None
     Return:
-      result: Boolean, test if in the test server or deploy server. 
-              if in test server, return *True*, if in deploy server, 
+      result: Boolean, test if in the test server or deploy server.
+              if in test server, return *True*, if in deploy server,
               return *False*
     '''
     ip_address = socket.gethostbyname(socket.gethostname())
-    print(ip_address)
-    
+
     key_words = ['127', '192']
 
     result = False
