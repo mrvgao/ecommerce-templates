78e7b8b36365f3395b4f699dd90aeeee59b8f92a
renjie <renmjie@163.com>
2015-08-28 11:45:50 +0800

    add_qcrode.jpg

--- new_printer/designer/conf/website.py
+++ new_printer/designer/conf/website.py
@@ -7,15 +7,14 @@ works_execute = '%s/designer-works.html'%base_site
 setup = '%s/designer-setinfo.html'%base_site
 change_icon = '%s/change_icon.html'%base_site
 
-reason_failed = ['模型文件大',
-				 '错了，不是模型文件',
-				 '背景颜色']
-good_tags = ['戒指',
-			'吊坠','耳坠','手链','项链','胸针']
+reason_failed = [u'模型文件大',
+				 u'错了，不是模型文件',
+				 u'背景颜色']
+good_tags = [u'戒指',u'吊坠',u'耳坠',u'手链',u'项链',u'胸针']
 
-good_style = ['自然','富丽','典雅','青春','时尚']
+good_style = [u'青春洋溢',u'富丽典雅',u'自然亲切',u'时尚潮流']
 
-unexec_one = 2 #
-auditing_one = 2#
-unpassed_one = 2#
-publish_one = 2#
+unexec_one = 4 #
+auditing_one = 4#
+unpassed_one = 5#
+publish_one = 5#
