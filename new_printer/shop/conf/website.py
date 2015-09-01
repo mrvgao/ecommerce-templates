4953cefacf0dbc51f789d77161905176a810400e
Wellaiyo <liwanglin_1994@163.com>
2015-09-01 11:21:49 +0800

    clean up the urls.py in shop

--- new_printer/shop/conf/website.py
+++ new_printer/shop/conf/website.py
@@ -1,12 +1,15 @@
 list = 'shop/list.html'
 
+test = 'shop/test.html'
+
 goods_detail = 'shop/goods-detail.html'
 
-test = 'shop/index.html'
+index = 'shop/index.html'
+
+home = 'shop/home.html'
+
 all_goods_list = 'shop/list.html'
 
 login_register = 'shop/sign.html'
 
 password_find = 'shop/password-find.html'
-
-index = 'shop/customer-service-for-window.html'
