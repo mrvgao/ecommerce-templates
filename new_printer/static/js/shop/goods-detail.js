f6fb7f9648a72e006402bfd5b2969d838bbe0091
strikerzj <strikerzj@163.com>
2015-09-02 14:35:27 +0800

    common.js modified by white

--- new_printer/static/js/shop/goods-detail.js
+++ new_printer/static/js/shop/goods-detail.js
@@ -115,14 +115,29 @@ $(function (){
 			g_img = goods_img.find('img');
 		if(_this.hasClass('slider-img')){
 			g_img.attr('src',_src);
+			$('#show-3d').hide();
 		}else if(_this.hasClass('watch3d-img')){
 			// 
-			var stlId = '';
-			showStlFileInRemoteServer(stlId , 458, 458, 'show-3d');
+			var stlId = GetRequest();
+			console.log('id:'+stlId);
+			showStlFileInRemoteServer(stlId , 460, 460, 'show-3d');
+			$('#show-3d').show();
 		}
 		airm_before.removeClass('active');
 		_this.addClass('active');
 	});
+
+
+	// added by white: get params in localhref
+	function GetRequest() {
+		var url = location.search; //获取url中"?"符后的字串
+		if (url.indexOf("?") != -1) {    //判断是否有参数
+			var str = url.substr(1); //从第一个字符开始 因为第0个是?号 获取所有除问号的所有符串
+			strs = str.split("=");   //用等号进行分隔 （因为知道只有一个参数 所以直接用等号进分隔 如果有多个参数 要用&号分隔 再用等号进行分隔）
+			/*alert(strs[1]);          //直接弹出第一个参数 （如果有多个参数 还要进行循环的）*/
+			return strs[1];
+		}
+	}
 
 });
 
