3c24ebcae9b7e722a439d31fcebc9b779a409726
chenjiankum <kkltcjk@gmail.com>
2015-09-02 11:23:15 +0800

    none

--- new_printer/shop/views.py
+++ new_printer/shop/views.py
@@ -383,11 +383,9 @@ def paging(request):
     tags_name  = request.POST['list_type'].strip()
     sort_name = request.POST['filter_type'].strip()
     style_name = request.POST['classify_type'].strip()
-    pdb.set_trace()
     start = page_now * per_page_num
     end = (page_now + 1) * per_page_num
     goods_list = common_filter(tags_name, sort_name, style_name)[start:end]
-    print 'nimie'
 
     context = {
         'goods_list': goods_list,
