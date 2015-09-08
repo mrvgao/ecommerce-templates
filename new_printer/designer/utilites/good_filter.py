61cabfc5abb97ca6ee9c9e06035b7e6db2c9472e
root <kkltcjk@gmail.com>
2015-09-08 18:08:33 +0800

    ggggg

--- new_printer/designer/utilites/good_filter.py
+++ new_printer/designer/utilites/good_filter.py
@@ -154,11 +154,10 @@ def publish_exec(good_list):
 #下载STL 文件到本地,以便预览stl
 def down_stl(_url):
     stl_path = "%s/"%pwd
-
     local_filename = _url.split('/')[-2]
     r = requests.get(_url, stream=True)
     lists = os.listdir(stl_path)
-    aleady_h = _url.split('/')[-1]
+    aleady_h = _url.split('/')[-2]
     stl_path = stl_path + local_filename
     stl_path = stl_path.decode("utf-8")
     if aleady_h in lists :
